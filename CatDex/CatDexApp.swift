9feb8089f67eabfe2b5eaccd119d185158963845
Hugo Paulista <hugo.paulista@foton.la>
2022-09-16 23:10:00 -0300

    Initial Commit

---
+++ CatDex/CatDexApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatDexApp.swift
+//  CatDex
+//
+//  Created by Hugo Paulista on 16/09/22.
+//
+
+import SwiftUI
+
+@main
+struct CatDexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
